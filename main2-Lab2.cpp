45d8c0ddc23d727315132b759ac715fe13f6b86b
jossuartlara-ops <jossuartlara1@gmail.com>
2025-09-25 19:49:19 -0400

    Add files via upload

---
+++ main2-Lab2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "What is your name?";
+    cin >> name;
+    
+    string age;
+
+    cout << "How old are you?";
+    cin >> age;
+    
+    string major;
+
+    cout<< "What is your Major?";
+    cin >> major;
+    
+return 0;
+}
